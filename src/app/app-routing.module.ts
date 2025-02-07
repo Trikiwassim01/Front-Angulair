9d00c65b00e65e9d6361df445e4e75119c68d042
Trikiwassim01 <wassimtriki098@gmail.com>
2025-02-07 23:09:23 +0100

    Updated README and fixed bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenberFormComponent } from './menber-form/menber-form.component';
+import { MemberComponent } from './member/member.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', 
+    pathMatch: 'full',
+    component: MenberFormComponent},
+    {
+      path: '',
+      pathMatch: 'full',
+      component: MemberComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
